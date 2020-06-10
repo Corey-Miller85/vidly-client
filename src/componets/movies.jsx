3f04644f51622987000c34d9b74d107693973da1
Corey Miller <coreyMiller353@gmail.com>
2020-06-09 21:18:39 -0400

    add pagination for all movies

--- src/componets/movies.jsx
+++ src/componets/movies.jsx
@@ -2,12 +2,15 @@ import React, { Component } from "react";
 import { getMovies } from "../services/fakeMovieService";
 import Like from "./common/like";
 import Pagination from "./pagination";
+import { paginate } from "../utils/paginate";
+import { getGenres } from "../services/fakeGenreService";
 
 class Movies extends Component {
 	state = {
 		movies: getMovies(),
 		pagesSize: 4,
 		currentPage: 1,
+		genres: getGenres(),
 	};
 
 	handleDelete = (movie) => {
@@ -31,61 +34,64 @@ class Movies extends Component {
 
 	render() {
 		const { length: count } = this.state.movies;
-		const { pagesSize, currentPage } = this.state;
-		if (this.state.movies.length === 0) {
+		const { pagesSize, currentPage, movies: allMovies } = this.state;
+
+		if (count === 0) {
 			return <p>There are no movies in the database.</p>;
-		} else {
-			return (
-				<React.Fragment>
-					<p>Showing {count} movies in the database.</p>
-					<table className='table'>
-						<thead>
-							<tr>
-								<th>Title</th>
-								<th>Genre</th>
-								<th>Stock</th>
-								<th>Rate</th>
-								<th></th>
-								<th></th>
-							</tr>
-						</thead>
-						<tbody>
-							{this.state.movies.map((movie, index) => {
-								return (
-									<tr key={movie._id}>
-										<td>{movie.title}</td>
-										<td>{movie.genre.name}</td>
-										<td>{movie.numberInStock}</td>
-										<td>{movie.dailyRentalRate}</td>
-										<td>
-											<Like
-												liked={movie.liked}
-												onClick={() => this.handleLike(movie)}
-											/>
-										</td>
-										<td>
-											<button
-												type='button'
-												className='btn btn-danger btn-sm'
-												onClick={() => this.handleDelete(movie)}
-											>
-												Delete
-											</button>
-										</td>
-									</tr>
-								);
-							})}
-						</tbody>
-					</table>
-					<Pagination
-						itemsCount={count}
-						pagesSize={pagesSize}
-						onPageChange={this.handlePageChange}
-						currentPage={currentPage}
-					/>
-				</React.Fragment>
-			);
 		}
+
+		const movies = paginate(allMovies, currentPage, pagesSize);
+
+		return (
+			<React.Fragment>
+				<p>Showing {count} movies in the database.</p>
+				<table className='table'>
+					<thead>
+						<tr>
+							<th>Title</th>
+							<th>Genre</th>
+							<th>Stock</th>
+							<th>Rate</th>
+							<th></th>
+							<th></th>
+						</tr>
+					</thead>
+					<tbody>
+						{movies.map((movie) => {
+							return (
+								<tr key={movie._id}>
+									<td>{movie.title}</td>
+									<td>{movie.genre.name}</td>
+									<td>{movie.numberInStock}</td>
+									<td>{movie.dailyRentalRate}</td>
+									<td>
+										<Like
+											liked={movie.liked}
+											onClick={() => this.handleLike(movie)}
+										/>
+									</td>
+									<td>
+										<button
+											type='button'
+											className='btn btn-danger btn-sm'
+											onClick={() => this.handleDelete(movie)}
+										>
+											Delete
+										</button>
+									</td>
+								</tr>
+							);
+						})}
+					</tbody>
+				</table>
+				<Pagination
+					itemsCount={count}
+					pagesSize={pagesSize}
+					onPageChange={this.handlePageChange}
+					currentPage={currentPage}
+				/>
+			</React.Fragment>
+		);
 	}
 }
 
--- src/componets/pagination.jsx
+++ src/componets/pagination.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import _ from "lodash";
+import PropTypes from "prop-types";
 
 const Pagination = (props) => {
 	const { itemsCount, pagesSize, onPageChange, currentPage } = props;
@@ -23,6 +24,13 @@ const Pagination = (props) => {
 			</ul>
 		</nav>
 	);
+};
+
+Pagination.propTypes = {
+	itemsCount: PropTypes.number.isRequired,
+	pagesSize: PropTypes.number.isRequired,
+	onPageChange: PropTypes.func.isRequired,
+	currentPage: PropTypes.number.isRequired,
 };
 
 export default Pagination;
